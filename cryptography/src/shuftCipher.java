909de75ad18588e5e16114da22d46365b65feda3
anshul-rohilla4 <anshulrohilla111@gmail.com>
2025-08-06 02:51:29 +0530

    shift cipher complete

--- cryptography/src/shuftCipher.java
+++ cryptography/src/shuftCipher.java
@@ -8,30 +8,56 @@ public class shuftCipher {
         //C = E(Pi) = (Pi+key)mod26  ( P(i) indicates the character of plain text at an index i)
         //Pi = D(Ci) = (Ci-key)mod26;
 
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Choose operation :");
+        System.out.println("1. Plain text  -> Cipher text");
+        System.out.println("2. Cipher text -> Plain text");
+        int choice = sc.nextInt();
+
+
         String string = "abcdefghijklmnopqrstuvwxyz";
 
         Map<Character, Integer> map = new HashMap<>();
         for (char c = 'a'; c <= 'z'; c++) {
             map.put(c, c - 'a');//A=0 ,...Z=25
         }
-        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the integer for your KEY");
         int key = sc.nextInt();
 
         String result = "";
 
-        System.out.println("Enter the plain text.");
-        String plainT = sc.next();
-        sc.close();
 
-        System.out.println("Plain text: " + plainT);
+        if(choice ==1) { //plain text to cipher text
+            System.out.println("Enter the plain text.");
+            String plainT = sc.next();
+            sc.close();
 
-        for (int i = 0; i < plainT.length(); i++) {
-            int Pi = map.get(plainT.charAt(i));
-            int num = (Pi + key) % 26;
-            result += string.charAt(num);
+            System.out.println("Plain text: " + plainT);
+
+            for (int i = 0; i < plainT.length(); i++) {
+                int Pi = map.get(plainT.charAt(i));
+                int num = (Pi + key) % 26;
+                result += string.charAt(num);
+            }
+            System.out.println("Cipher text: " + result);
+            result="";
         }
-        System.out.println("Cipher text: " + result);
+        else { //cipher text to plain text
+            System.out.println("Enter the cipher text");
+            String CipherT=sc.next();
+            sc.close();
+
+            System.out.println("Cipher text: "+CipherT);
+
+            for(int i=0;i<CipherT.length();i++){
+                int Ci=map.get(CipherT.charAt(i));
+                int num=((Ci-key) % 26 + 26)%26;
+                result += string.charAt(num);
+            }
+            System.out.println(result);
+            result="";
+        }
 
     }
 }
